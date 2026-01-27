7482f98d7aba0e06bd6dde03d0c3231d6e587d3c
Lex-fabian <alexugsha87@gmail.com>
2026-01-27 09:00:58 -0500

    Fix undefined esAdmin middleware in reserva routes

--- backend/routes/reserva.routes.js
+++ backend/routes/reserva.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const reservaController = require('../controllers/reserva.controller');
-const { verificarToken, esAdmin } = require('../middleware/auth');
+const { verificarToken, esAdminOSuper } = require('../middleware/auth');
 
 router.use(verificarToken);
 
@@ -9,8 +9,8 @@ router.post('/', reservaController.crear);
 router.get('/', reservaController.obtenerTodas);
 router.get('/:id', reservaController.obtenerPorId);
 router.put('/:id', reservaController.actualizar);
-router.patch('/:id/confirmar', esAdmin, reservaController.confirmar);
+router.patch('/:id/confirmar', esAdminOSuper, reservaController.confirmar);
 router.patch('/:id/cancelar', reservaController.cancelar);
-router.delete('/:id', esAdmin, reservaController.eliminar);
+router.delete('/:id', esAdminOSuper, reservaController.eliminar);
 
 module.exports = router;
